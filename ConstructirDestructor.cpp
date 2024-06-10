e600e2cd383c953ae0446fb37a39aa548cb354da
ChoiHanggil <choihg0930@gmail.com>
2024-06-10 17:57:12 +0900

    6/10 수업

--- ConstructirDestructor.cpp
+++ ConstructirDestructor.cpp
@@ -7,6 +7,8 @@ public:
 	float mY;
 
 	//Constructir(생성자)
+	//기본 생성자 MyClass() = default;
+	MyClass() = default;
 	MyClass(int x, int y)
 	{
 		mX = x;
@@ -16,8 +18,50 @@ public:
 	~MyClass()
 	{
 	}
+	
+	//POD : Plain Old Data;
+	//C언어의 타입 (int, float...)과 구조체는 POD입니다.
 };
+//-------------------------------------------------------
+class Mouse
+{
+private:
+	int mPrice;
+	int mDpi;
 
+public:
+	Mouse()
+	{
+		mPrice = mDpi = 0;
+	}
+	Mouse(int price, int dpi = 100)
+	{
+		mPrice = price;
+		mDpi = dpi;
+	}
+};
+//-------------------------------------------------
+
+//멤버 초기화 목록(member initialize list)
+class Keyboard
+{
+private:
+	int mPrice;
+	int mKeys;
+	const int mType;
+
+public:
+	//멤버 초기화 목록 - <생성자> :  <초기화식1>, <초기화식2>...
+	Keyboard(int price, int keys, int type) : 
+		mPrice(price), mKeys(keys), mType(type)
+		//멤버 순서 대로 초기화
+	{
+	}
+	//대리 생성자
+	Keyboard() :
+	{
+	}
+};
 int main()
 {
 	//변수의 선언과 정의
@@ -32,5 +76,9 @@ int main()
 	MyClass p = MyClass(1, 1);
 	MyClass p2(2, 2);
 	MyClass p3{ 2, 2 };
-
+//-----------------------------------------
+	Mouse m1{}; // uniform initialization(생성자를 부르는 식)
+	Mouse m2(100,300);
+	Mouse m3(100);
+//--------------------------------------
 }
